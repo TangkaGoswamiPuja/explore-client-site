8a6daad64672ec89035d15fbb622c08427713ef9
TangkaGoswamiPuja <web4dev2learning0@gmail.com>
2024-04-30 15:42:10 +0600

    we

--- src/Pages/HomeElement/Tourists.jsx
+++ src/Pages/HomeElement/Tourists.jsx
@@ -7,6 +7,16 @@ const tour = useLoaderData();
 // console.log(_id)
     return (
         <div>
+   <div className="hero-content text-center ml-10">
+    <div className="max-w-md">
+      <h1 className="text-4xl font-semibold "> <span className='text-orange-500'>BEST </span>Place For YOU</h1>
+      <p className=" text-2xl font-semibold ">We are ready to go with your<br></br> dream place</p>
+      
+    </div>
+  </div>
+
+
+            
          <div className="card card-compact w-96 bg-base-100 shadow-xl">
   <figure><img src="https://i.ibb.co/WfW8ZCF/pexels-pixabay-531602.jpg" alt="" /></figure>
   <div className="card-body">
